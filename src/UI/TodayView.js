b976f815cad2a7496d072775fc691101fa368a49
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2023-06-02 01:27:27 -0500

    add loop to gather today tasks

--- src/UI/TodayView.js
+++ src/UI/TodayView.js
@@ -1,23 +1,31 @@
 export default class TodayView {
   htmlDisplay;
-  constructor(todayList, projectColor) {
+  constructor(todayDate, todayList) {
     console.log('called constructor TodayView');
     let todayWrapper = document.createElement('div');
     todayWrapper.className = 'viewChild';
 
     let taskItems = ""
-    todayList.forEach(task => {
-      taskItems += this.createTaskItem(task);
+    todayList.forEach(item => {
+      taskItems += this.createTaskItem(item[0], item[1]);
     });
 
     todayWrapper.innerHTML += `
-    
-    `
+      <div class="todayTitle">
+        <div>Due Today</div>
+        <div>${todayDate}</div>
+      </div>
+      <div class="taskListWrapper">
+          <ul class="tasksList">${taskItems}</ul>
+      </div>
+    `;
+
+    this.htmlDisplay = todayWrapper;
   }
 
   createTaskItem = (task, projectColor) => {
     let taskItemHTML = `
-        <li class="taskItem" style="background-color:${projectColor}">
+        <li class="taskItem taskItemToday" style="background-color:${projectColor}">
             <input type="checkbox" class="taskItemCheckbox">
             <div>${task.title}</div>
             <div>Edit</div>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import LocalStorage from "./Logics/LocalStorage";
 import './style.css';
 import layout from "./UI/layout";
 import ProjectView from "./UI/ProjectView";
+import TodayView from "./UI/TodayView";
 
-var project1 = new Project("Odin1", "red")
+const todayDateObj = new Date();
+
+var project1 = new Project("Odin1", "#ffafcc")
 project1.tasks = [1,1,2];
 
 console.log(project1.title);
@@ -19,13 +22,13 @@ var dueDate = new Date("17 May 2023 5:00:00");
 var task1 = new Task("Planning logic 1", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
 console.log(task1.dueDateAndTime, task1.isCompleted);
 
-var dueDate = new Date("20 May 2023 5:00:00");
+dueDate = new Date("20 May 2023 5:00:00");
 var task2 = new Task("Planning logic 2 ", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
-console.log(task2.dueDateAndTime, task1.isCompleted);
+console.log(task2.dueDateAndTime, task2.isCompleted);
 
-var dueDate = new Date("25 May 2023 5:00:00");
+dueDate = new Date("25 May 2023 5:00:00");
 var task3 = new Task("Planning logic 3", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
-console.log(task3.dueDateAndTime, task1.isCompleted);
+console.log(task3.dueDateAndTime, task3.isCompleted);
 
 project1.tasks = [task1, task3, task2];
 console.log(project1.tasks);
@@ -40,7 +43,22 @@ console.log(project1.tasks);
 project1.removeTask(task0.title, task0.dueDateAndTime);
 console.log(project1.tasks);
 
-var project2 = new Project("Odin2", "blue");
+var project2 = new Project("Odin2", "#a2d2ff");
+
+var dueDate = new Date("2 June 2023 5:00:00");
+var task4 = new Task("Planning logic 4", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
+console.log(task4.dueDateAndTime, task4.isCompleted);
+
+dueDate = new Date("20 May 2023 5:00:00");
+var task5 = new Task("Planning logic 2 ", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
+console.log(task5.dueDateAndTime, task5.isCompleted);
+
+dueDate = new Date("25 May 2023 5:00:00");
+var task6 = new Task("Planning logic 3", "Write down things", dueDate)  //Month is May but we're passing 4 because counter starting at 0 - Jan
+console.log(task6.dueDateAndTime, task6.isCompleted);
+
+project2.tasks = [task4, task5, task6];
+
 
 var projects = [];
 
@@ -48,10 +66,29 @@ var storage = new LocalStorage(projects);
 console.log(storage.projects);
 storage.removeProject("Odin3");
 
-var project3 = new Project("Odin3", "yellow");
+var project3 = new Project("Odin3", "#fcf6bd");
 storage.addProject(project3);
 storage.removeProject("Odin3");
 console.log(storage.projects);
+storage.addProject(project2);
+storage.addProject(project1);
+console.log('projects in storage:', storage.projects);
+console.log(storage.projects.length);
+console.log(storage.projects[0].tasks.length);
+
+var todayTasks = [];
+
+for (let i = 0; i < storage.projects.length; i++) {
+  for (let j = 0; j < storage.projects[i].tasks.length; j++) {
+    if (storage.projects[i].tasks[j].dueDateAndTime.toLocaleDateString() == todayDateObj.toLocaleDateString())
+    {
+      todayTasks.push([storage.projects[i].tasks[j], storage.projects[i].color]);
+      console.log(todayTasks);
+    }
+    console.log("no matched date");
+  }
+}
+
 /* End of logic test */
 
 contentDiv.appendChild(layout);
@@ -63,6 +100,10 @@ let projectView2 = new ProjectView(project1.title, project1.color, project1.task
 console.log(projectView1);
 view.appendChild(projectView1.htmlDisplay);
 view.appendChild(projectView2.htmlDisplay);
+
+let todayView = new TodayView(todayDateObj.toLocaleDateString(), todayTasks);
+console.log(todayView);
+view.appendChild(todayView.htmlDisplay);
 
 //function to hide/show taskList
 function toggleList() {
